a6f6be547397d41f5e3adcd966cec03387c46705
user <user@usernoMacBook-Pro.local>
2023-11-24 16:13:40 +0900

    authentication finished

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useReducer } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
@@ -29,15 +30,18 @@ function AuthProvider({ children }) {
     reducer,
     initialState
   );
+  const navigate = useNavigate();
+
   function login(email, password) {
     if (email === FAKE_USER.email && password === FAKE_USER.password)
       dispatch({ type: "login", payload: FAKE_USER });
   }
   function logout() {
+    navigate("login");
     dispatch({ type: "logout" });
   }
   return (
-    <AuthContext.Provider value={(user, isAuthenticated, login, logout)}>
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
@@ -47,6 +51,7 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside the Auth Provider");
+  return context;
 }
 
 export { AuthProvider, useAuth };
---
+++ src/pages/ProtectedRoutes.jsx
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect } from "react";
+
+export default function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/");
+  }, [isAuthenticated, navigate]);
+
+  //Since the component renders beffore the effect, we might see if the user is authenticated
+  return isAuthenticated ? children : null;
+}
