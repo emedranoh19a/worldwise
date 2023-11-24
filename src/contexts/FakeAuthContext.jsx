39f8e1d15e1aa64cfa484959acf81281ec3980ab
user <user@usernoMacBook-Pro.local>
2023-11-24 14:50:25 +0900

    Authentication Part 1 Complete: Set up Context and CUstom hook together. Get user's email & password and check with an API endpoint (this time, there is no API)

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,9 +1,46 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
 function AuthProvider({ children }) {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+  return (
+    <AuthContext.Provider value={(user, isAuthenticated, login, logout)}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
@@ -11,3 +48,5 @@ function useAuth() {
   if (context === undefined)
     throw new Error("AuthContext was used outside the Auth Provider");
 }
+
+export { AuthProvider, useAuth };
